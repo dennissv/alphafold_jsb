44e3dc55b03916e8081985bae50a4c362ebd9c7a
Dennis Svedberg <dennis@danhog.molbiol.umu.se>
2021-09-27 13:57:18 +0200

    Save MSAs in seperate folder

--- run_mmseqs2.py
+++ run_mmseqs2.py
@@ -21,17 +21,23 @@ proteins = [(name, seq) for name, seq in proteins.items()]
 proteins.sort(key=lambda p: len(p[1]))
 proteins = proteins[1000:1800]
 result_dir = 'ready'
-os.makedirs(result_dir, exist_ok=True) 
+os.makedirs(result_dir, exist_ok=True)
+os.makedirs('msas', exist_ok=True)
 ready = set([f for f in os.listdir(result_dir) if isfile(join(result_dir, f))])
 
 for name, seq in proteins[:5]:
     start = time.time()
     print("Running: " + name)
-    try:  
-        a3m_lines = cf.run_mmseqs2(seq, name, True)
+    try:
+        a3m_lines = cf.run_mmseqs2(seq, join('msas', name), True)
     except:
         print(name + " could not be processed")
         continue
-    os.mknod(join(result_dir, name))
+    try:
+        os.mknod(join(result_dir, name))
+    except:
+        pass
     end = time.time()
+    if end - start < 10:
+        continue
     time.sleep(max(0, 300 - (end-start)))
